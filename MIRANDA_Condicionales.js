ea67036277a875f621f9363d083e1a20c8d2ad3f
Abi Miranda <83624380+abimiranda@users.noreply.github.com>
2022-02-25 12:07:41 -0300

    Add files via upload

---
+++ MIRANDA_Condicionales.js
@@ -0,0 +1,37 @@
+// PRACTICA ABIGAIL MIRANDA
+
+/*var altura = 53
+
+if(altura > 52){
+    console.log("¡Súbete, chico!")
+}else{
+    console.log("Lo siento, chico. Tal vez el próximo año")
+}*/
+
+
+    //STRETCH 1 Ajusta la sentencia condicional para requerir tanto la altura como la edad. 
+    //sobre este código, declaré las variables en ese valor para que las condiciones se cumplen 
+
+
+var altura = 53 // la variabole se puede modificar y ver el resultado en la consola
+var edad = 11 // la variable se puede modificar y ver el resultado en la consola
+
+if(altura > 52 && edad > 10){ // si la altura es mayor a 52 Y la edad es mayor a 10
+    console.log("¡Súbete, chico!") // se verá en la consola el mensaje
+}else{                              // sino
+    console.log("Lo siento, chico. Tal vez el próximo año") // se verá en la consola el mensaje
+}
+
+/**********************************************************************************************/
+
+                                        //STRETCH 2
+// en el codigo que escribí la unica condicion que se cumple es la edad.
+
+/*var altura = 52 // la variabole se puede modificar y ver el resultado en la consola
+var edad = 11 // la variable se puede modificar y ver el resultado en la consola
+
+if(altura > 52 || edad > 10){ // si la altura es mayor a 52 Y la edad es mayor a 10
+    console.log("¡Súbete, chico!") // se verá en la consola el mensaje
+} else {                              // sino
+    console.log("Lo siento, chico. Tal vez el próximo año") // se verá en la consola el mensaje
+}*/
